bb50b70328ea289f1af5fb5edc9b43d40d96ceb7
sebastiendewitodisee <sebastien.dewit@student.odisee.be>
2026-02-08 13:23:40 +0100

    chore: add build stamp to verify deployed version

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
-﻿import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { BUILD_INFO } from "../buildInfo";
 
 function Footer() {
   const { t } = useTranslation();
@@ -8,6 +9,7 @@ function Footer() {
     <footer className="site-footer">
       <div className="container footer-inner">
         <p>{t("footer.copyright", { year: new Date().getFullYear() })}</p>
+        <p className="build-stamp">build: {BUILD_INFO}</p>
         <Link to="/legal" className="footer-link">
           {t("footer.legal")}
         </Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-﻿import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { BUILD_INFO } from "./buildInfo";
 import "./i18n";
 import "./styles/global.css";
 import "./styles/components.css";
+
+console.log("PATATOS_BUILD_INFO", BUILD_INFO);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
